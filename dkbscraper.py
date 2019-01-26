7e1dc13b7b27dd57a47c0ec572e85a7ec9f9bca3
pmarszal <marszal@yahoo.de>
2019-01-26 16:10:49 +0100

    Updated Urls & Transaction collection.

--- dkbscraper.py
+++ dkbscraper.py
@@ -13,6 +13,8 @@ import tempfile
 import shutil
 import os
 import os.path
+import datetime
+import time
 
 
 PostboxDocument = collections.namedtuple(
@@ -41,8 +43,8 @@ class DKBSession(object):
     >>> dkbs.logout()
     """
 
-    base_url = 'https://banking.dkb.de'
-    login_url = '/dkb/-?$javascript=disabled'
+    base_url = 'https://www.dkb.de'
+    login_url = '/-?$javascript=disabled'
 
     def __init__(self, verbose=True):
 
@@ -64,7 +66,14 @@ class DKBSession(object):
         login_page = lxml.html.fromstring(r.text)
 
         # Get DKB Banking login form
-        login_form = login_page.forms[0]
+        login_form = None
+        for i,f in enumerate(login_page.forms):
+            if 'j_username' in f.fields.keys():
+                login_form=f
+                break
+        else:
+            raise RuntimeError('Login Form not found.')
+
 
         # Fill in username and password
         login_form.fields['j_username'] = username
@@ -77,13 +86,14 @@ class DKBSession(object):
         )
 
         # Parse returned page
-        soup = bs4.BeautifulSoup(r.text)
+        soup = bs4.BeautifulSoup(r.text, features="lxml")
 
         if not soup.find(text='Finanzstatus'):
             raise RuntimeError('Login to DKB Online Banking failed.')
 
         self.logout_url = soup.find('a', id='logout')['href']
-        self.postbox_url = soup.find(id="valueOut")['href']
+        self.postbox_url = soup.find(tid="Postfach").a['href']
+        self.umsatz_url = soup.find(tid='Umsaetze').a['href']
 
         if self.verbose:
             print('Logged in to DKB Online Banking')
@@ -113,7 +123,7 @@ class DKBSession(object):
         r = self.s.get(self.base_url + self.postbox_url)
 
         # Parse Postbox page
-        soup = bs4.BeautifulSoup(r.text)
+        soup = bs4.BeautifulSoup(r.text, features="lxml")
 
         # Get document table
         table = soup.find(id="documentsTableOverview_outer")
@@ -178,3 +188,68 @@ class DKBSession(object):
         os.unlink(tmp_filename)
 
         return True
+
+    def get_banking_accounts(self):
+        """
+        Gather the keys and names of all linked accounts.
+        """
+        url = self.base_url + self.umsatz_url
+
+        r = self.s.get(url)
+
+        soup = bs4.BeautifulSoup(r.text, features="lxml")
+        slAllAccounts = soup.find(tid='slAllAccounts')
+        self.accounts = {}
+        for account in slAllAccounts.find_all('option'):
+            self.accounts[account['value']] = account.text
+
+    def get_all_banking_account_transactions(self,start_date, end_date, destination="./"):
+        """
+        Download the transactions for the given time interval in csv form.
+        """
+        assert type(start_date)==datetime.date, "Dates have to be in datetime.date format"
+
+        start_date_str = start_date.strftime("%d.%m.%Y")
+        end_date_str = end_date.strftime("%d.%m.%Y")
+
+        url = self.base_url + self.umsatz_url
+
+        r = self.s.get(url)
+
+        selection_page = lxml.html.fromstring(r.text)
+
+        select_form = None
+        for i,f in enumerate(selection_page.forms):
+            if "slAllAccounts" in f.fields.keys():
+                select_form=f
+                break
+        else:
+            raise RuntimeError("Could not find selection forms.")
+
+        for o in list(self.accounts.keys()):
+            if not 'PayPal' in self.accounts[o]:
+                select_form.fields['slAllAccounts'] = o
+                select_form.fields['searchPeriodRadio'] = '1'
+                select_form.fields['transactionDate'] = start_date_str
+                select_form.fields['toTransactionDate'] = end_date_str
+
+                r = self.s.post(self.base_url + select_form.action, data=dict(select_form.fields))
+
+
+                soup = bs4.BeautifulSoup(r.text, features="lxml")
+
+                csv_url = soup.find('a',tid='csvExport')['href']
+
+                r = self.s.get(self.base_url+csv_url)
+
+                fout = open(
+                        destination+"ac%s_%s_to_%s.csv"%(
+                            o,
+                            start_date_str,
+                            end_date_str
+                            ),
+                        'w'
+                        )
+                fout.write(r.text)
+                fout.close()
+            time.sleep(5)
